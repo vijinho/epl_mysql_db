2e7cc716e904195cece18b466a1b5e3101b19876
Vijay Mahrra <vijay.mahrra@gmail.com>
2017-09-16 05:09:11 +0100

    updated queries to show all wins for all teams with totals

--- sql/queries/most_away_wins.sql
+++ sql/queries/most_away_wins.sql
@@ -1,7 +1,6 @@
--- Top 10 teams with most away wins
 SELECT AwayTeam, COUNT(*) AS wins
 FROM EPL
 WHERE FTHG < FTAG
 GROUP BY AwayTeam
-ORDER BY COUNT(*) DESC
-LIMIT 10;
+ORDER BY wins DESC
+;
--- sql/queries/most_home_wins.sql
+++ sql/queries/most_home_wins.sql
@@ -1,7 +1,6 @@
--- Top 10 teams with most home wins
 SELECT HomeTeam, COUNT(*) AS wins
 FROM EPL
 WHERE FTHG > FTAG
 GROUP BY HomeTeam
-ORDER BY COUNT(*) DESC
-LIMIT 10;
+ORDER BY wins DESC
+;
